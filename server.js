e36abe044a0cddbc91ef67b7ce9752222a030776
Gimy404 <77947018+Gimy404@users.noreply.github.com>
2024-04-09 22:37:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,30 +6,25 @@ const nodemailer = require('nodemailer');
 const app = express();
 const port = 3000;
 
-// إزالة الاستدعاء السابق لـ app.use(cors()) لتجنب تعارض الإعدادات
-// تمكين طلبات CORS لمجال محدد
-app.use(cors({
-  origin: 'https://test-mail-kv8xucvgr-mohamed-gamals-projects-da8ddc00.vercel.app'
-}));
+app.use(bodyParser.json());
+app.use(cors());
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
   auth: {
-    user: 'homos.kitsaway@gmail.com', // تأكد من تغيير هذه البيانات أو استخدام متغيرات البيئة
-    pass: 'mnmf skua gzfl nmln' // تأكد من تغيير هذه البيانات أو استخدام متغيرات البيئة
+    user: 'homos.kitsaway@gmail.com',
+    pass: 'mnmf skua gzfl nmln'
   }
 });
-
-app.use(bodyParser.json());
 
 app.post('/send-email', (req, res) => {
   const { userName, userNumber, userEmail, userMessage } = req.body;
 
   const mailOptions = {
-    from: 'homos.kitsaway@gmail.com', // تأكد من تغيير هذا البريد الإلكتروني
-    to: 'm.gamal.zaid1@gmail.com', // تأكد من تغيير هذا البريد الإلكتروني
+    from: 'homos.kitsaway@gmail.com',
+    to: 'm.gamal.zaid1@gmail.com',
     subject: 'بيانات المستخدم',
     text: `الاسم: ${userName}\nالرقم: ${userNumber}\nالبريد الإلكتروني: ${userEmail}\nالتعليقات/الأسئلة: ${userMessage}`
   };
